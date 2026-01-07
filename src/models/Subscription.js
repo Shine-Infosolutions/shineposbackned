1d6f93613f49a23906d5360941d108467aad0358
Anshu sharma <156065826+Anshusharma42019@users.noreply.github.com>
2026-01-07 15:55:56 +0530

    updates

--- src/models/Subscription.js
+++ src/models/Subscription.js
@@ -17,7 +17,7 @@ const subscriptionSchema = new mongoose.Schema(
 
     status: {
       type: String,
-      enum: ["active", "expired"],
+      enum: ["active", "expired"], 
       default: "active"
     },
 
