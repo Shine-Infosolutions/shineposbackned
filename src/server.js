00fd16bf74115c00168071d6eb32d2de7aeddd62
Anshu sharma <156065826+Anshusharma42019@users.noreply.github.com>
2026-01-08 17:38:46 +0530

    updates timebesedSubscription

--- src/server.js
+++ src/server.js
@@ -98,4 +98,5 @@ app.use('*', (req, res) => {
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
+  console.log('TimeBasedSubscriptionService module error - FIXED');
 });
